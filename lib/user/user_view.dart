578c9cda145c0fdaaa677bf42299c3bafa0c19c8
igy-apps <islammohy@gmail.com>
2023-10-13 23:32:43 +0300

    modified:   lib/main.dart 	modified:   lib/user/user_view.dart

--- lib/user/user_view.dart
+++ lib/user/user_view.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:mvc_provider/user/user_controller.dart';
 import 'package:mvc_provider/user/user_model.dart';
@@ -20,32 +22,36 @@ class _UserViewState extends State<UserView> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Provider.of<UserController>(context).users.isNotEmpty
-                  ? Column(
-                      children: [
-                        Selector<UserController, String>(
+              Container(
+                child: ListView.builder(
+                    scrollDirection: Axis.vertical,
+                    shrinkWrap: true,
+                    itemCount:
+                        Provider.of<UserController>(context).users.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return ListTile(
+                          //  leading: const Icon(Icons.list),
+                          trailing: InkWell(
+                            child: Icon(Icons.delete),
+                            onTap: () {
+                              Provider.of<UserController>(context,
+                                      listen: false)
+                                  .removeUser(index);
+                            },
+                          ),
+                          title: Selector<UserController, String>(
                             builder: (context, value, child) => Text(value),
-                            selector: (context, usermodel) =>
-                                usermodel.users[0].name),
-                        Selector<UserController, int>(
-                            builder: (context, value, child) =>
-                                Text('Age: $value'),
-                            selector: (context, usermodel) =>
-                                usermodel.users[0].age),
-                        MaterialButton(
-                          onPressed: () {
-                            Provider.of<UserController>(context, listen: false)
-                                .increaseAge(0);
-                          },
-                          child: const Text('Increase Age'),
-                        ),
-                      ],
-                    )
-                  : const SizedBox(),
+                            selector: (context, userController) =>
+                                userController.users[index].name,
+                          ));
+                    }),
+              ),
               MaterialButton(
                 onPressed: () {
-                  Provider.of<UserController>(context, listen: false)
-                      .addUser(UserModel(name: 'GG', age: 50));
+                  Provider.of<UserController>(context, listen: false).addUser(
+                      UserModel(
+                          name: 'Gen Name ${1 + Random().nextInt(100 - 1 + 1)}',
+                          age: 50));
                 },
                 child: const Text('Add User'),
               )
